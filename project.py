cbb363d34fab1eac58dbe36d5316b48bebcb7ad7
stressors <stressors05@gmail.com>
2025-12-11 03:42:30 +0700

    Update project.py

--- project.py
+++ project.py
@@ -934,10 +934,10 @@ def safe_display_square_image(path):
 images_dir = "images"
 os.makedirs(images_dir, exist_ok=True)
 placeholder_files = [
-    os.path.join(images_dir, "gitsi.jpg"),
-    os.path.join(images_dir, "bella.jpg"),
-    os.path.join(images_dir, "chinta.jpg"),
-    os.path.join(images_dir, "yessa.jpg"),
+    os.path.join(images_dir, "gitsi.jpeg"),
+    os.path.join(images_dir, "bella.jpeg"),
+    os.path.join(images_dir, "chinta.jpeg"),
+    os.path.join(images_dir, "yessa.jpeg"),
 ]
 for p in placeholder_files:
     if not os.path.exists(p):
@@ -1478,10 +1478,10 @@ st.markdown(t["team_title"])
 st.write(t["team_subtitle"])
 
 members = [
-    {"img": "images/gitsi.jpg", "name": "Gita Sion Nauli Simatupang", "sid": "004202400055", "role": "Leader", "Contribution": "Project Manager, Geometric Transformations Module"},
-    {"img": "images/bella.jpg", "name": "Bella Amelia", "sid": "004202400050", "role": "Member", "Contribution": "Image Filtering Module, UI/UX Design"},
-    {"img": "images/chinta.jpg", "name": "Chinta Amanda Dwi Putri Carelina", "sid": "004202400035", "role": "Member", "Contribution": "Background Removal Module, Image Upload & Download"},
-    {"img": "images/yessa.jpg", "name": "Yessa Kireina Hanna Sevira", "sid": "004202400009", "role": "Member", "Contribution": "Histogram Module, Image Processing Functions"},
+    {"img": "images/gitsi.jpeg", "name": "Gita Sion Nauli Simatupang", "sid": "004202400055", "role": "Leader", "Contribution": "Project Manager, Geometric Transformations Module"},
+    {"img": "images/bella.jpeg", "name": "Bella Amelia", "sid": "004202400050", "role": "Member", "Contribution": "Image Filtering Module, UI/UX Design"},
+    {"img": "images/chinta.jpeg", "name": "Chinta Amanda Dwi Putri Carelina", "sid": "004202400035", "role": "Member", "Contribution": "Background Removal Module, Image Upload & Download"},
+    {"img": "images/yessa.jpeg", "name": "Yessa Kireina Hanna Sevira", "sid": "004202400009", "role": "Member", "Contribution": "Histogram Module, Image Processing Functions"},
 ]
 
 cols_row1 = st.columns(1, vertical_alignment="top")
@@ -1541,3 +1541,4 @@ for i in range(1, 4):
                 f"<div style='text-align:center;'>{t['team_contribution']} {m['Contribution']}</div>",
                 unsafe_allow_html=True,
             )
+
